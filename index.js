79c7c5f88496d6e972fbb181baec20e892551fda
adelina2222 <adelinachiriac@gmail.com>
2021-08-20 14:43:58 +0100

    Require Express & create a test request and listener

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello!");
+});
+
+app.listen("3000", () => {
+    console.log("Listening...");
+});
