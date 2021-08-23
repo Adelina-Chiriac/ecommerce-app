ce16dce9ac23c1a6d2d97d44c372037ee7ee7100
adelina2222 <adelinachiriac@gmail.com>
2021-08-23 16:17:34 +0100

    Require the body parser middleware function & use it for all route handlers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send(`
@@ -9,10 +12,15 @@ app.get("/", (req, res) => {
             <input name="email" placeholder="Email" />
             <input name="password" placeholder="Password" />
             <input name="passwordConfirmation" placeholder="Password Confirmation" />
-            <button>Sign Up</button>s
+            <button>Sign Up</button>
         </form>
     </div>
     `);
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("Your account has been created!");
 });
 
 app.listen("3000", () => {
