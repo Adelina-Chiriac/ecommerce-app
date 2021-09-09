9859d02efa4ee0a700197f88faa4421f1f8d971b
adelina2222 <adelinachiriac@gmail.com>
2021-09-09 11:12:51 +0100

    Handle the corner case of the user not having an existing cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
 
-router.post("/cart/products", (req, res) => {
-    console.log(req.body.productId);
+router.post("/cart/products", async (req, res) => {
+    // If the user doesn't have an existing cart
+    if (!req.session.cartId) {
+        const cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    }
 
     res.send("Product added to cart!");
 });
