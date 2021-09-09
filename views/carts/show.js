e96bf4986a86a0a1a263bbf95bbae688eaa12165
adelina2222 <adelinachiriac@gmail.com>
2021-09-09 15:05:10 +0100

    Small change made to the cart template

--- views/carts/show.js
+++ views/carts/show.js
@@ -3,7 +3,7 @@ const layout = require("../layout");
 module.exports = ({ items }) => {
     const renderedProducts = items.map((item) => {
         return `
-            <div>${item.product.title} - ${item.product.price}</div>
+            <div>${item.product.title} - $${item.product.price} | Quantity: ${item.quantity}</div>
         `;
     }).join("");
 
