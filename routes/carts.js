a61597457d703451378efd1350af6ef22b7284da
adelina2222 <adelinachiriac@gmail.com>
2021-09-09 14:09:40 +0100

    Write the GET request handler to show all the products in the cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const showCartTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
+// POST request for adding products to cart
 router.post("/cart/products", async (req, res) => {
     let cart;
     // If the user doesn't have an existing cart
@@ -32,6 +35,24 @@ router.post("/cart/products", async (req, res) => {
     });
 
     res.send("Product added to cart!");
+});
+
+// GET request to show all the products in the cart
+router.get("/cart", async (req, res) => {
+    // If the user doesn't have an existing cart, redirect to homepage
+    if (!req.session.cartId) {
+        return res.redirect("/");
+    }
+    // Fetch the user's cart
+    const cart = await cartsRepo.getOne(req.session.cartId);
+    // Iterate through the products in the cart & look them up in the products repository
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+        // Add the product info to the item variable for building the cart template
+        item.product = product;
+    }
+
+    res.send(showCartTemplate({ items: cart.items }));
 });
 
 module.exports = router;
